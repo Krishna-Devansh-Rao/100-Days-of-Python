b0211207492c52cdc89160ea66d133b1229e9d13
KRISHNA DEVANSH RAO <100765720+Krishna-Devansh-Rao@users.noreply.github.com>
2025-04-25 13:32:22 +0530

    Create D-1.py

---
+++ D-1.py
@@ -0,0 +1,47 @@
+#                       ###DAY - 1 ###
+
+# ##Strings###
+
+# print("Hello World")#
+# Hello World#
+
+# print("Hey there! \n Let's play a game of Rock, Paper, Scissors!")#
+# Hey there!
+# Let's play a game of Rock, Paper, Scissors!
+
+# print("Krishna" + " Devansh " + "Rao ")
+# Krishna Devansh Rao
+
+# INDENTATIONERROR (YADI GALTI SE SPACE DE THO SENTNCE KE STARTING MAI  ERROR AAYEGA )))
+
+# ##Input Function###
+
+# input("What is your name?") #input likhne ke baad jo bhi likhenge wo input ke roop mai aayega aur aage ans likhne ka option aayega
+
+# input(A prompt for the user) #input ke baad jo bhi likhenge wo prompt ke roop mai aayega)
+
+# print("Hello " + input("What is your name?") + "!") #pehle ques puchega aur phir uss ques ka ans input ke roop mai aayega aur phir uss ans ke saath Hello print ho jayega
+
+# ## Variables ###
+# name = input("What is your name?")
+# print( "Hello " + name)###
+
+# a = "krishna"
+# print(a)
+
+# ##Length Function###
+
+# a = "krishna"
+# b = "devansh"
+# c = "rao"
+# print(len(a) * len(b) + len(c))
+
+# ###Project Day -1 ###
+
+# print("Welcome to the Bussiness name Generator")
+# a = input("What is the first name of your bussiness ? \n")
+# print("Okay so " + a + " is the first name of your bussiness")
+# b = input("What is the last name of your bussiness ? \n")
+# print("Your bussiness name is " + (a + " " + b))
+
+
