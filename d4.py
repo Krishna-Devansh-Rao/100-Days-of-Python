aaba46f28784bbac156ad161fcf868f718d63b4b
KRISHNA DEVANSH RAO <100765720+Krishna-Devansh-Rao@users.noreply.github.com>
2025-06-23 19:32:28 +0530

    d4.py

---
+++ d4.py
@@ -0,0 +1,35 @@
+#                           ###Day - 4 ###
+
+# import random
+# import fav
+
+# random_integer = random.randint(1, 15)
+# print(random_integer)
+
+# print(fav.RCB_VK)
+
+# random_heads_tails = random.randint(0, 1)
+# if random_heads_tails == 1:
+#   print("Heads")
+# else:
+#     print("Tails")
+
+
+# import random
+
+# random_no = random.randint(1, 50)
+# print(random_no)
+
+
+                          ###Lists
+
+# Fruits = ["Apple", " Grapes" , "Mango ","Banana", "Cherry"]
+# print(Fruits[2])
+
+# Fruits.append("Orange")
+# print(Fruits)
+
+
+                          ### Project - 4 ###
+
+
