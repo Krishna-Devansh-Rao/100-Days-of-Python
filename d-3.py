28ea71371e505ad4ff49343de68c18a8dc115359
KRISHNA DEVANSH RAO <100765720+Krishna-Devansh-Rao@users.noreply.github.com>
2025-06-25 08:37:01 +0530

    250625_D-3.py

--- d-3.py
+++ d-3.py
@@ -1,89 +1,60 @@
-# # # # # # # # Conditions 
+# # Conditional
 
-# # # #                             ### if/else ###
+# if condition:
+#     do this
+# else :
+#     do this
 
-# # # # # # # if condition: 
-# # # # # # #   do this
-# # # # # # # else condition:
-# # # # # # #   do this
+# Operators
+# > Greater than
+# < Less than
+# >= Greater than equal to 
+# <= Less than equal to
+# == equal to 
+# != Not equal to 
 
-# # # #                             ### experiment -1
+                                        ### experiment -1
 
-# # # # # print("Welcome to the rollercoaster!")
-# # # # # height = int(input("what is your height in cm? "))
-# # # # # if height >= 120:
-# # # # #   print("You can ride the rollercoaster!")
-# # # # # else  :
-# # # # #   print("Sorry, you have to grow taller before you can ride.")
+# print("Welcome to roller coaster")
+# height = (int(input("What is your height in cm ?")))
+# bill = 0
+
+
+# # if height >= 120 :
+#     bill = 5
+#     print("Your can enjoy the ride")
+#     age = int(input("What is your age ?"))
+
+#     if age <= 18 :
+#         bill = 7
+#         print('Please Pay 7$')
+    
+#     elif age >= 22:
+#         bill = 12
+#         print('Please Pay 12$')
+    
+#     else:
+#         bill = 15
+#         print('Please Pay 15$')
+#     photo = input("Do you want a photo taken? Y  for yes or N for No ")
+
+#     if photo == "Y":
+#         print("Please pay $3 more")
+
+#     bill += 3
+    
+#     print("SO your final bill is $", bill, "!")
+    
+# else:
+    # print("Sorry ! Yoube have to grow taller to take this ride")
 
 
 # # # #                           ### Modulo Operator ( % ) ### ( Remainder ))
 
-# # # # # # # #  Modulo Operator ( % ) ###
-
-# # # # # 12 % 2 == 0
-# # # # # print(28 % 13 ) 
-
-# # # weight = 85
-# # # height = 1.85
-
-# # # bmi = weight / (height ** 2)
-
-# # # # 🚨 Do not modify the values above
-# # # # Write your code below 👇
-
-# # # # if bmi >= 18.5 :
-# # # #     print("underweight")
-
-# # # # elif bmi >= 18.5 :
-# # # #     print("normal weight")
-
-# # # # else :
-# # # #     print("overweight") 
-
-# # # if bmi >= 25:
-# # #     print("overweight")
-# # # elif bmi >= 18.5:
-# # #     print("normal weight")
-# # # else:
-# # #     print("underweight")
+# print(10%4)
 
 
-
-# # print("Welcome to the rollercoaster!")
-# # height = int(input("what is your height in cm? "))
-# # bill = 0
-# # if height >= 120:
-# #   print("You can ride the rollercoaster!")
-# #   age = int(input("what is your age? "))
-# #   if age < 12:
-# #     print("Please pay $5")
-# #     bill = 5
-# #   elif age <= 18:
-# #     print("Please pay $7")
-# #     bill = 7
-# #   else:
-# #     print("Please pay $12")
-# #     bill = 12
-
-# #   photo = input("Do you want a photo taken? Y  for yes or N for No ")
-# #   if photo == "Y":
-# #     print("Please pay $3 more")
-
-# #     bill += 3
-    
-# #     print("SO your final bill is $", bill, "!")
-    
-   
-  
-    
-# # else  :
-# #   print("Sorry, you have to grow taller before you can ride.")
-
-#                         ###Experiment -2 ###
-
-# from typing import TYPE_CHECKING
-
+                                        ### experiment - 2
 
 # print("Welcome to Python pizza Deliveries!")
 # size = input("What size pizza do you want? S for Small, M for Medium, or L for Large ")
@@ -125,22 +96,40 @@
 
 # print("Your final bill is: $", bill + cheese + pepperoni)
 
+                    # Project --------------->>>>>>>>>>>>>>>>>>>>> 3
+# Tresure island
 
-                      
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/[TomekK]
+*******************************************************************************
+      ''')
 
-
-
-
-
-                        ### Logical Operators ###
-
-
-                      #Project 3 - Treasure Island
-
-
-
-
-
+print('''
+ _                                     _     _                 _ 
+| |                                   (_)   | |               | |
+| |_ _ __ ___  __ _ ___ _   _ _ __ ___ _ ___| | __ _ _ __   __| |
+| __| '__/ _ \/ _` / __| | | | '__/ _ \ / __| |/ _` | '_ \ / _` |
+| |_| | |  __/ (_| \__ \ |_| | | |  __/ \__ \ | (_| | | | | (_| |
+ \__|_|  \___|\__,_|___/\__,_|_|  \___|_|___/_|\__,_|_| |_|\__,_|
+''')
 
 left = "left"
 wait = "wait"
